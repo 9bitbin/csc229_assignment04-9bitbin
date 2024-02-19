c8a283768b14010475b509cd8f17f1054660e869
Himal Shrestha <Shresthahimal12@yahoo.com>
2024-02-18 22:30:48 -0500

    modified code to only print even numbers

--- src/main/java/org/example/MainClass.java
+++ src/main/java/org/example/MainClass.java
@@ -1,21 +1,28 @@
 public class MainClass {
     public static void main(String[] args) {
-
+        countDown(10); // Start countdown from 10
     }
 
     static void countDown(int num) {
-        if (num == 0)// test
+        if (num == 0) {
             System.out.println("Blastoff!");
-        else {
-            System.out.println("..");
-            countDown(num - 1);// recursive call
+        } else {
+            if (num % 2 == 0) { // Check if num is even
+                System.out.println(num);
+            }
+            countDown(num - 1); // Recursive call
         }
     }
 }
+
 //Task 01:
 /**
  * Since the countdown method is never called within main the code will not produce output when executed
  * the countdown emthod is defined but not invoked so the prgram will terminate without printing
  * anything
  */
+/**
+ * printd only even numbers
+ */
+
 
